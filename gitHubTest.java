6acb6d9dbb95adc83fc6c0c7f4263589fc367c2a
kbladow83716 <kevin@bladow.us>
2024-05-20 10:32:47 -0600

    this is the real deal

---
+++ gitHubTest.java
@@ -0,0 +1,11 @@
+package week03;
+
+public class gitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is only a GitHUb Test");
+		System.out.println("If this had been a real assignment, you would have been instructed on where to go in your area.");
+	}
+
+}
